93f45910d5933d6c3ef8da0a3882e5e8d3990d2f
Marwan <chaerul.marwan@weekendinc.com>
2021-10-17 15:35:23 +0700

    rename preset-1 to preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: [],
   darkMode: "class",
-  presets: [require("./preset-1")],
+  presets: [require("./preset")],
   theme: {
     extend: {
       container: { center: true, padding: "1rem" },
